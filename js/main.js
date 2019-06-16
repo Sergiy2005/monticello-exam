ca8170ba27134d56a2350cc1cf204f4594c2b88b
Sergiy2005 <sergiy.gogol@gmail.com>
2019-06-16 11:14:16 +0300

    final update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-
 function initSlider(){
     console.log("start");
+    $('.my-first-slider').slick({
+        dots: true,
+        vertical: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
     $('.my-slider').slick({
         dots: true,
         infinite: true,
